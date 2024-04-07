3d271b91e87ebd02a8dd8efa86d1f4cfb8446e0a
Gautam <gautam72120@gmail.com>
2024-04-07 10:59:30 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "MTIyMzcyMzg3MDkzMTEyODQzMg.G7eamd.0fibEqX4giBy5L2pIQxrhhpG4tl1gxUTyRKmqE"
+
+REDIS_URI = "redis://default:V225i0xUYEswxcPZgdJDHrltLyKXMzWO@redis-13232.c301.ap-south-1-1.ec2.cloud.redislabs.com:13232"
+
+DATABASE_PATH = "mongodb+srv://gautam72120:gOvonKSVheiFBBYb@plasma.yh9v9vz.mongodb.net/"
+DATABASE_NAME = "plasma"
